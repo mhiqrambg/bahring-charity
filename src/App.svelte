bc6070e0eb910ca87cbc15d61cafc995888f9bc7
Muh. Iqram Bahring <muhiqrambahring@gmail.com>
2022-09-04 16:13:26 +0800

    Convert HTML to svelte components

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,4 @@
-<h1> Charity</h1>
+<script>
+    import Home from "./pages/Home.svelte";
+</script>
+<Home />
---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'SUMBANGAN PENDIDIKAN',
+            category: ' Pendidikan',
+            thumbnail: 'https://images.pexels.com/photos/5212359/pexels-photo-5212359.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            pledged: 9000,
+            target: 30000,
+            date_end: new Date('28 september 2022'),
+            profile_photo: 'https://images.pexels.com/photos/6994925/pexels-photo-6994925.jpeg?cs=srgb&dl=pexels-julia-m-cameron-6994925.jpg&fm=jpg',
+            profile_name: 'Muh Iqram Bahring',
+            no_pledges: 0
+        }
+    ]
+}
