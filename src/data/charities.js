1c52ef179256f08e48408c03b36d5448535f093f
Muh. Iqram Bahring <muhiqrambahring@gmail.com>
2022-09-04 23:21:44 +0800

    Convert HTML to svelte components

--- src/data/charities.js
+++ src/data/charities.js
@@ -4,7 +4,7 @@ module.exports = {
             id: 1,
             title: 'SUMBANGAN PENDIDIKAN',
             category: ' Pendidikan',
-            thumbnail: 'https://images.pexels.com/photos/5212359/pexels-photo-5212359.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            thumbnail: 'https://i.ibb.co/QQv3zjV/mhiqrambg.png',
             pledged: 9000,
             target: 30000,
             date_end: new Date('28 september 2022'),
