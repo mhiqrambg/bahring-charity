5f402f8982448d5539e7ca297d437fd2e3d14b19
Muh. Iqram Bahring <78313274+mhiqrambg@users.noreply.github.com>
2023-10-04 17:37:23 +0800

    Update charities.js
    change code expired end charity

--- src/data/charities.js
+++ src/data/charities.js
@@ -7,7 +7,7 @@ module.exports = {
             thumbnail: 'https://i.ibb.co/QQv3zjV/mhiqrambg.png',
             pledged: 9000,
             target: 30000,
-            date_end: new Date('28 september 2022'),
+            date_end: new Date('28 september 2025'),
             profile_photo: 'https://images.pexels.com/photos/6994925/pexels-photo-6994925.jpeg?cs=srgb&dl=pexels-julia-m-cameron-6994925.jpg&fm=jpg',
             profile_name: 'Muh Iqram Bahring',
             no_pledges: 0
